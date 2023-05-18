17dbf1c0ae819876b35837ecaabe53f5444291e6
Jimmy Chen <jimmychen260@gmail.com>
2023-05-18 11:25:00 +0800

    feat: use clean architecture to user domain (#2)

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-  "e-commerce-with-chatgpt/usermanagement"
-  "fmt"
+	"e-commerce-with-chatgpt/usermanagement"
+	"fmt"
 )
 
 func main() {
-	um := usermanagement.NewUserManager()
+	repo := &MockUserRepository{
+		users: make(map[string]User),
+	}
+	uc := NewUserUseCase(repo)
 
 	// Test case 1: Creating a new user with valid data
-	userData := usermanagement.UserData{
+	userData := UserData{
 		Username: "john_doe",
 		Email:    "john.doe@example.com",
 		Password: "password123",
 	}
 
-	createdUser, err := um.CreateUser(userData)
+	createdUser, err := uc.CreateUser(userData)
 	if err != nil {
-	  fmt.Println("create user fail")
+		fmt.Println("create user fail")
 	}
 	fmt.Println(createdUser.Username)
 	fmt.Println(createdUser.Email)
--- usermanagement/user_management.go
+++ usermanagement/user_management.go
@@ -1,6 +1,8 @@
 package usermanagement
 
-import "errors"
+import (
+	"errors"
+)
 
 // UserData represents the data required to create a new user
 type UserData struct {
@@ -15,28 +17,33 @@ type User struct {
 	Email    string
 }
 
-// UserManager is responsible for user management operations
-type UserManager struct {
-	users map[string]User // Assuming username is unique
+// UserRepository defines the interface for interacting with the user repository
+type UserRepository interface {
+	Create(user User) error
+	FindByUsername(username string) (User, error)
 }
 
-// NewUserManager creates a new instance of UserManager
-func NewUserManager() *UserManager {
-	return &UserManager{
-		users: make(map[string]User),
+// UserUseCase represents the user management use cases
+type UserUseCase struct {
+	userRepository UserRepository
+}
+
+// NewUserUseCase creates a new instance of UserUseCase
+func NewUserUseCase(userRepository UserRepository) *UserUseCase {
+	return &UserUseCase{
+		userRepository: userRepository,
 	}
 }
 
 // CreateUser creates a new user with the provided data
-func (um *UserManager) CreateUser(userData UserData) (User, error) {
+func (uc *UserUseCase) CreateUser(userData UserData) (User, error) {
 	// Check if the username already exists
-	if _, ok := um.users[userData.Username]; ok {
+	_, err := uc.userRepository.FindByUsername(userData.Username)
+	if err == nil {
 		return User{}, errors.New("username already exists")
 	}
 
 	// Validate the email format
-	// Note: This is a simple validation, you can use a more comprehensive email validation library
-	// or implement your own validation logic
 	if !isValidEmail(userData.Email) {
 		return User{}, errors.New("invalid email format")
 	}
@@ -48,15 +55,18 @@ func (um *UserManager) CreateUser(userData UserData) (User, error) {
 	}
 
 	// Save the user
-	um.users[userData.Username] = user
+	err = uc.userRepository.Create(user)
+	if err != nil {
+		return User{}, errors.New("failed to create user")
+	}
 
 	return user, nil
 }
 
 // AuthenticateUser authenticates a user with the provided credentials
-func (um *UserManager) AuthenticateUser(username, password string) (User, error) {
-	user, ok := um.users[username]
-	if !ok {
+func (uc *UserUseCase) AuthenticateUser(username, password string) (User, error) {
+	user, err := uc.userRepository.FindByUsername(username)
+	if err != nil {
 		return User{}, errors.New("user not found")
 	}
 
